7797e84ea4a0442e9ed5b4bd8598ac40065e5255
oskarhertzman <43958723+oskarhertzman@users.noreply.github.com>
2020-03-24 21:36:07 +0100

    Added a11y tabbing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
             <div className="container__body">
               <div className="textfield">
                 <div className="textfield__group">
-                    <input type="text" required/>
+                    <input type="text"  required tabindex="0"/>
                     <span className="textfield__group__bar"></span>
                     <label>Textfield focused</label>
                   </div>
@@ -22,30 +22,29 @@ function App() {
                   </div>
                 </div>
               <div className="switch">
-                  <input type="checkbox" id="switch__enabled" className="switch__input" />
-                  <label for="switch__enabled" className="switch__label">Switch enabled</label>
+                  <input type="checkbox" id="switch__enabled" className="switch__input" tabindex="0" />
+                  <label htmlFor="switch__enabled" className="switch__label">Switch enabled</label>
                 </div><br></br>
               <div className="switch">
                   <input type="checkbox" id="switch__disabled" className="switch__input" disabled />
-                  <label for="switch__disabled" className="switch__label">Switch disabled</label>
+                  <label htmlFor="switch__disabled" className="switch__label">Switch disabled</label>
                 </div><br></br>
               <div className="checkbox">
-                  <input type="checkbox" id="checkbox__focused" className="checkbox__input" />
-                  <label for="checkbox__focused" className="checkbox__label">Checkbox focused</label>
+                  <input type="checkbox" id="checkbox__focused" className="checkbox__input" tabindex="0"/>
+                  <label htmlFor="checkbox__focused" className="checkbox__label">Checkbox focused</label>
               </div><br></br>
               <div className="checkbox">
                   <input type="checkbox" id="checkbox__disabled" className="checkbox__input" disabled />
-                  <label for="checkbox__disabled" className="checkbox__label">Checkbox disabled</label>
+                  <label htmlFor="checkbox__disabled" className="checkbox__label">Checkbox disabled</label>
               </div><br></br>
               <div className="radio">
-                <input type="radio" id="radio__checked" className="radio__input"/>
-                <label for="radio__disabled" className="radio__label">Checkbox disabled</label>
+                <input type="radio" id="radio__checked" className="radio__input" tabindex="0"/>
+                <label htmlFor="radio__checked" className="radio__label">Checkbox checked</label>
               </div><br></br>
               <div className="radio">
                 <input type="radio" id="radio__disabled" className="radio__input" disabled />
-                <label for="radio__disabled" className="radio__label">Checkbox disabled</label>
+                <label htmlFor="radio__disabled" className="radio__label">Checkbox disabled</label>
               </div>
-
           </div>
         </div>
       </div>
